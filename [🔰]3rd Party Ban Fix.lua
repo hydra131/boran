18c6d61133950a6358865c4807448963ed84fff9
hydra131 <85440093+hydra131@users.noreply.github.com>
2021-06-12 21:24:11 +0300

    Add files via upload

---
+++ [🔰]3rd Party Ban Fix.lua
@@ -0,0 +1,6 @@
+gg.clearResults()--TELEGRAM = https://t.me/trhydra
+gg.setRanges(gg.REGION_C_ALLOC)--TELEGRAM =https://t.me/trhydra
+gg.searchNumber(":/data/user/0/com.tencent", gg.TYPE_BYTE)--TELEGRAM =https://t.me/trhydra
+gg.getResults(99999)--TELEGRAM = https://t.me/trhydra
+gg.editAll("116", gg.TYPE_BYTE)--TELEGRAM = https://t.me/trhydra
+gg.clearResults()--TELEGRAM = https://t.me/trhydra
